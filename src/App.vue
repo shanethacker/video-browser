018f032a21f504462fe9db197e0a061615da4ef3
Shane Thacker <sst@clockworkmind.com>
2019-01-24 16:06:58 -0500

    Added Youtube search.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
     <div>
         <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList :videos="videos"></VideoList>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
 const API_KEY = process.env.VUE_APP_API_KEY;
 
 export default {
     name: 'App',
     components: {
-        SearchBar: SearchBar,
+        SearchBar,
+        VideoList,
+    },
+    data() {
+        return { videos: [] };
     },
     methods: {
         onTermChange(searchTerm) {
-            console.log(searchTerm);
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => {
+                this.videos = response.data.items;
+            });
+
         },
     }
 };
